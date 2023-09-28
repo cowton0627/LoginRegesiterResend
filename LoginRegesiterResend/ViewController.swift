c1ccdabfbbc686aaec152ab2ab0ea6f23a359ee1
Charles Cheng <charles_cheng@microjet.com.tw>
2023-09-28 13:22:49 +0800

    Initial Commit

---
+++ LoginRegesiterResend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginRegesiterResend
+//
+//  Created by Chun-Li Cheng on 2023/9/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
